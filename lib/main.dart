fc5eb2e805a2aaa00ed307d8f20f72889e01879d
Roger Oliveira <rogerpolvr@gmail.com>
2019-10-26 12:33:38 -0300

    Adicionando um background e trocando o label pessoas para clientes

--- lib/main.dart
+++ lib/main.dart
@@ -3,36 +3,51 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       title: "People Counter",
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      home: Stack(
         children: <Widget>[
-          Text("Pessoas: 0",
-              style:
-              TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-          Row(
+          Image.asset(
+            "images/background.jpg",
+            fit: BoxFit.cover,
+            height: 1000.0,
+          ),
+          Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              FlatButton(
-                child: Text(
-                  "+1",
-                  style: TextStyle(color: Colors.white, fontSize: 40),
-                ),
-                onPressed: () {},
+              Text("Clientes: 0",
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: FlatButton(
+                      child: Text(
+                        "+1",
+                        style: TextStyle(color: Colors.white, fontSize: 40),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: FlatButton(
+                      child: Text(
+                        "-1",
+                        style: TextStyle(color: Colors.white, fontSize: 40),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
               ),
-              FlatButton(
-                child: Text(
-                  "-1",
-                  style: TextStyle(color: Colors.white, fontSize: 40),
-                ),
-                onPressed: () {},
-              )
+              Text("Pode Entrar",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30.0)),
             ],
-          ),
-          Text("Pode Entrar",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30.0)),
+          )
         ],
       )));
 }
